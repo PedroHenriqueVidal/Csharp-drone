44e808450596007f79d4ee2903fd0302d5525cbe
Pedro Henrique <pvidal123016@gmail.com>
2024-09-11 15:16:08 -0300

    Consertado arquivo Program.cs

--- DroneRobo/Program.cs
+++ DroneRobo/Program.cs
@@ -1,16 +1,17 @@
-﻿using DroneRobo;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MenuUI;
 
+namespace MyApp;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("Controle do Drone");
         new Menu();
     }
 
